af54d3cac6f82ead9f16944aabc1880235e88987
Oleg <mrb4el@hotmail.com>
2016-05-31 13:48:37 +0600

    First client started
    subj

---
+++ Client/Client/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    public class ClassTimer
+    {
+        public ClassTimer() { }
+
+        private int current_time;
+
+        public int status = 0; // 0 - stop, 1 - pause, 2 - working
+
+        public void counter()
+        {
+
+        }
+    }
+
+    public class IsPinCodeSetCheck
+    {
+        public bool GetStatus()
+        {
+            bool result = false;
+
+            //Frame.Navigate(typeof(Page2));
+
+            return result;
+        }
+    }
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        ClassTimer Timer = new ClassTimer();
+
+        private void button_action_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            switch (Timer.status)
+            {
+                case 0:
+                    Timer.counter();
+                    break;
+
+                case 1:
+                    break;
+
+                default:
+                    break;
+
+            }
+
+        }
+    }
+
+}
---
+++ Client/Client/PinAuth.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Шаблон элемента пустой страницы задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App1
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class PinAuth : Page
+    {
+        public PinAuth()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_PIN_Click(object sender, RoutedEventArgs e)
+        {
+            int pin = Int32.Parse(textBox_PIN.Text);
+
+            //check(pin);
+
+            Frame.Navigate(typeof(MainPage), pin);
+        }
+    }
+}
